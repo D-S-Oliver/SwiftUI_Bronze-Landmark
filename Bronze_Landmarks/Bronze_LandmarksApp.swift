4905eb1ab748206688a6328e5c1b4ac0e58606fc
Oliver (Bruno) <ukyio.sun@gmail.com>
2021-10-25 15:04:56 -0300

    Initial Commit

---
+++ Bronze_Landmarks/Bronze_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bronze_LandmarksApp.swift
+//  Bronze_Landmarks
+//
+//  Created by Bruno Neves Oliveira on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Bronze_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
